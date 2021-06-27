a29114f97f50b5afcda3b45835cb21c6787e2ea3
Gatsjy <gkswndks123@naver.com>
2021-06-27 21:43:10 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 글자를 변경해보자.
+            lbl1.Text = "글자입니다.";
+            lbl2.Text = "글자2.";
+        }
+
+        // 버튼을 클릭했을 때 동작하는 함수
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbl3.Text = "글자3";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("체크박스1을 클릭하였습니다.");
+        }
+    }
+}
---
+++ WindowsFormsApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 해당 애플리케이션의 주 진입점입니다.
+        /// 프로젝트는 하나의 프로그램으로 생각하면 됩니다.
+        /// </summary>
+        [STAThread]
+        static void Main() // CTRL+F5
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
